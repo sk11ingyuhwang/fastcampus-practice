d1918343d34de336be1b8af1462bf2edb53b7182
Uno Kim <djkehh@gmail.com>
2022-02-09 06:01:42 +0900

    Apply #2 - lombok
    롬복 적용
    필수 생성자 애노테이션으로 생성자 코드를 줄이는 예시

--- src/main/java/com/fastcampus/practice/controller/MainController.java
+++ src/main/java/com/fastcampus/practice/controller/MainController.java
@@ -1,20 +1,20 @@
 package com.fastcampus.practice.controller;
 
 import com.fastcampus.practice.service.StringSortService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+
+@RequiredArgsConstructor
 @RestController
 public class MainController {
 
     private final StringSortService stringSortService;
 
-    public MainController(StringSortService stringSortService) {
-        this.stringSortService = stringSortService;
-    }
 
     @GetMapping("/")
     public List<String> main(@RequestParam List<String> list) {
