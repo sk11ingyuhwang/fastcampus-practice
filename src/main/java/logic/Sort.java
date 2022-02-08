b95788f8b2bf87bb200275505165bd0448c69c9b
Uno Kim <djkehh@gmail.com>
2022-02-08 14:33:55 +0900

    Add Sort interface
    각 정렬 구현체의 기능을 추상화하여 기능을  뽑아내고,
    interface 를 정의.
    각 구현 클래스는 Sort 인터페이스를 구현하게끔 작업.

---
+++ src/main/java/logic/Sort.java
@@ -0,0 +1,19 @@
+package logic;
+
+import java.util.List;
+
+/**
+ * 정렬 기능을 제공하는 인터페이스. 사용하는 타입은 정렬할 수 있게끔 {@link Comparable} 을 구현해야 한다.
+ *
+ * @param <T> 정렬할 데이터의 타입
+ */
+public interface Sort <T extends Comparable<T>> {
+
+    /**
+     * 정렬을 수행한다. 오름차순만 되니 주의.
+     *
+     * @param list 정렬할 리스트
+     * @return 정렬된 새 리스트
+     */
+    List<T> sort(List<T> list);
+}
