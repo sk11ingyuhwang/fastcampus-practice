6c6f616932bbf8c095b7741f7b83541cca5db9d4
sk11ingyuhwang <sk11ingyu@gmail.com>
2022-02-10 05:46:15 +0900

    feat: logger 추가

--- src/main/java/com/fastcampus/practice/controller/MainController.java
+++ src/main/java/com/fastcampus/practice/controller/MainController.java
@@ -3,6 +3,8 @@ package com.fastcampus.practice.controller;
 import com.fastcampus.practice.service.StringSortService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +17,7 @@ import java.util.List;
 public class MainController {
 
     private final StringSortService stringSortService;
-
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @GetMapping("/")
     public List<String> main(@RequestParam List<String> list) {
@@ -24,6 +26,13 @@ public class MainController {
 
     @GetMapping("/test1")
     public String test1(){
+
+        logger.trace("Trace Level 테스트");
+        logger.debug("DEBUG Level 테스트");
+        logger.info("INFO Level 테스트");
+        logger.warn("Warn Level 테스트");
+        logger.error("ERROR Level 테스트");
+
         long start = System.currentTimeMillis();
         long end = System.currentTimeMillis();
         log.warn("걸린 시간: {}ms", end-start);
