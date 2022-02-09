332468f7ac796d1837d431649ba3f0a9e378b0da
hwangingyu <sk11ingyu@gmail.com>
2022-02-09 12:31:19 +0900

    feat: logger 추가
    Logger, log 객체
    @Slf4j

--- src/main/java/com/fastcampus/practice/controller/MainController.java
+++ src/main/java/com/fastcampus/practice/controller/MainController.java
@@ -9,6 +9,11 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.juli.logging.Log;
+import org.apache.juli.logging.LogFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,17 +23,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 public class MainController {
 
-    @Value("${uno.config}")
-    private String hey;
-
+    private Logger logger = LoggerFactory.getLogger(MainController.class);
+    private final Log log = LogFactory.getLog(MainController.class);
     private final StringSortService stringSortService;
 
     @GetMapping("/")
     public String main(@RequestParam List<String> list) {
+        log.info("이것은 로그입니다.");
+        logger.info("나는 {}입니다.",list);
         return stringSortService.doSort(list).toString();
     }
 
