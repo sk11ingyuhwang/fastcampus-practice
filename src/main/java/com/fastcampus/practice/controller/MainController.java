0c5687d756e255b977614d5601d5104a1dc54001
hwangingyu <sk11ingyu@gmail.com>
2022-02-09 12:24:18 +0900

    feat: Spring EL

--- src/main/java/com/fastcampus/practice/controller/MainController.java
+++ src/main/java/com/fastcampus/practice/controller/MainController.java
@@ -1,23 +1,66 @@
 package com.fastcampus.practice.controller;
 
 import com.fastcampus.practice.service.StringSortService;
+import java.util.Objects;
+import javax.validation.Valid;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotBlank;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RequiredArgsConstructor
 @RestController
 public class MainController {
 
-    private final StringSortService stringSortService;
+    @Value("${uno.config}")
+    private String hey;
 
-    public MainController(StringSortService stringSortService) {
-        this.stringSortService = stringSortService;
-    }
+    private final StringSortService stringSortService;
 
     @GetMapping("/")
     public String main(@RequestParam List<String> list) {
         return stringSortService.doSort(list).toString();
     }
+
+    @PostMapping("/student")
+    public String student(@RequestBody @Valid Student student){
+        return student.name + ": " +student.age + "살";
+    }
+
+    public static class Student {
+        @NotBlank
+        private String name;
+        @Digits(fraction = 0, integer = 200) private String age;
+    }
+
+//    @RequiredArgsConstructor
+//    @EqualsAndHashCode
+//    @ToString
+//    @Getter
+//    public static class StudentClass {
+//        @NotBlank private String name;
+//        @Digits(fraction=0, integer = 4) private final String age;
+//    }
+//
+//    public void test(){
+//        StudentClass studentClass = new StudentClass("uno",18);
+//        studentClass.getAge();
+//        studentClass.getName();
+//        studentClass.toString();
+//
+//        Student student = new Student("uno",18);
+//        student.age();
+//        student.name();
+//        student.toString();
+//    }
 }
