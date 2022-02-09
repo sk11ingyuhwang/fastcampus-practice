3ea324546e09b69d79bff74cfeec5e8d39fa5731
sk11ingyuhwang <sk11ingyu@gmail.com>
2022-02-09 16:39:21 +0900

    feat: MainController 메소드 추가

--- src/main/java/com/fastcampus/practice/controller/MainController.java
+++ src/main/java/com/fastcampus/practice/controller/MainController.java
@@ -2,13 +2,14 @@ package com.fastcampus.practice.controller;
 
 import com.fastcampus.practice.service.StringSortService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 public class MainController {
@@ -19,5 +20,13 @@ public class MainController {
     @GetMapping("/")
     public List<String> main(@RequestParam List<String> list) {
         return stringSortService.doSort(list);
+    }
+
+    @GetMapping("/test1")
+    public String test1(){
+        long start = System.currentTimeMillis();
+        long end = System.currentTimeMillis();
+        log.warn("걸린 시간: {}ms", end-start);
+        return "걸린 시간 테스트";
     }
 }
