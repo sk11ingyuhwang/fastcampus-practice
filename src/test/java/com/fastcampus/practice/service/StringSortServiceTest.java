dfeb8ddd7092d250f86ec9d3d0f75e88f74bde5e
sk11ingyuhwang <sk11ingyu@gmail.com>
2022-02-10 12:39:44 +0900

    test: Mock을 주입받아서 테스트 코드 작성

--- src/test/java/com/fastcampus/practice/service/StringSortServiceTest.java
+++ src/test/java/com/fastcampus/practice/service/StringSortServiceTest.java
@@ -6,31 +6,58 @@ import com.fastcampus.practice.logic.JavaSort;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 //@ExtendWith(SpringExtension.class) ??
+//SpringBootTest
+//@WebMvcTest
+@AutoConfigureMockMvc
 @SpringBootTest
 class StringSortServiceTest {
 
-   // 먼가 들어가야 하는데?
-    private StringSortService sut;
+    private final MockMvc mvc;
 
-    public StringSortServiceTest(@Autowired StringSortService sut) {
-        this.sut = sut;
+
+    public StringSortServiceTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
     }
 
     @DisplayName("")
     @Test
     void test(){
         List<String> input = List.of("Hi", "My");
-        List<String> expected = sut.doSort(input);
-        assertThat(expected).isEqualTo(List.of("Hi", "My"));
+//        assertThat(expected).isEqualTo(List.of("Hi", "My"));
     }
 
+    @Test
+    void name() throws Exception {
+        List<String> input = List.of("1","2","3");
 
+//        ResultActions result = mvc.perform(
+//            get("/")
+//                .content("application/json")
+//                .param("list",String.join(",",input))
+//        );
+//
+//        result.andExpect(status().isOk())
+//            .andExpect((ResultMatcher) content().contentType(MediaType.APPLICATION_JSON))
+//            .andExpect(content().string("[\"0\",\"1\",\"2\",\"3\"]")
+//                .andDo(print()));
+
+        mvc.perform(
+            get("/")
+                .content("application/json")
+                .param("list",String.join(",",input))
+        ).andExpect(status().isOk())
+            .andExpect(content().contentType("application/json"))
+            .andExpect(content().string("[\"1\",\"2\",\"3\"]"))
+                .andDo(print());
+    }
 }
